6041bd6537406dd9e3e8f1f6d0911eae379e6394
Cclz <rex620620>
2017-05-04 10:20:00 +0800

    add Service

--- app/src/main/java/com/cclz/myapp_170504_01/MyService.java
+++ app/src/main/java/com/cclz/myapp_170504_01/MyService.java
@@ -2,13 +2,25 @@ package com.cclz.myapp_170504_01;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
 import android.util.Log;
 
+import java.util.Date;
+
 public class MyService extends Service {
+    Handler handler=new Handler();
     public MyService() {
     }
+
+    Runnable showTime=new Runnable() {
+        @Override
+        public void run() {
+            Log.d("SER1", "Time: " + new java.util.Date());
+            handler.postDelayed(this, 1000);
+        }
+    };
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -26,6 +38,7 @@ public class MyService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("SER1", "This is onStartCommand");
+        handler.post(showTime);
         return super.onStartCommand(intent, flags, startId);
     }
 
